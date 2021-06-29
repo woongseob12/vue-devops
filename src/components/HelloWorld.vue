c49773599e595ea8154906fa095725f6a6251790
woongseob <woongseob12@naver.com>
2021-06-30 00:43:09 +0900

    배포 연습

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="hello">
-    <h1>Vue 배포 연습</h1>
-    <p>배포 연습 끝!</p>
+    <h1>Vue 배포 연습 끝</h1>
   </div>
 </template>
 
@@ -10,20 +9,3 @@ export default {
   name: "HelloWorld",
 };
 </script>
-
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
